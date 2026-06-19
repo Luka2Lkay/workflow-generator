d5cf2c15715f0092aa5256a195a0d6a53d8823cc
Luka <luka.matshebelele@gmail.com>
2026-06-19 13:13:59 +0200

    include or

--- frontend/src/services/workflow_api.ts
+++ frontend/src/services/workflow_api.ts
@@ -1,13 +1,14 @@
 // import axios from "axios";
 // import { type Analysis } from "../interfaces/interface";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
-
+const BASE_URL =
+  import.meta.env.VITE_BASE_URL ||
+  "https://workflow-generator-backend.onrender.com";
 
 export const analyzeWorkflow = async (description: string) => {
   try {
-    console.log("workflow: ", description)
-    console.log("url :", BASE_URL)
+    console.log("workflow: ", description);
+    console.log("url :", BASE_URL);
     // const response = await axios.post<Analysis>(
     //   `${BASE_URL}/api/analyze`,
     //   {
