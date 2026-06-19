db2ed8298bb620e64c44271216e03e0ce34a5b46
Luka <luka.matshebelele@gmail.com>
2026-06-19 13:25:06 +0200

    add hardcoded url

--- frontend/src/services/workflow_api.ts
+++ frontend/src/services/workflow_api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { type Analysis } from "../interfaces/interface";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL =
+  import.meta.env.VITE_BASE_URL ||
+  "https://workflow-generator-backend.onrender.com";
 
 export const analyzeWorkflow = async (description: string) => {
   try {
