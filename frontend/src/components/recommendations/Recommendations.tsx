74d12f3ecd440618d905ee7d84103b27498028d9
Luka <luka.matshebelele@gmail.com>
2026-06-20 02:48:27 +0200

    center recommendations

--- frontend/src/components/recommendations/Recommendations.tsx
+++ frontend/src/components/recommendations/Recommendations.tsx
@@ -3,16 +3,16 @@ import { type RecommendationsProps } from "../../interfaces/interface";
 function Recommendations({ recommendations, tools = [], estimatedSavings }: RecommendationsProps) {
     return (
         <section className="bg-white rounded-xl shadow p-6">
-            <div className="mb-4">
+            <div className="mb-4 text-center">
                 <h2 className="text-xl font-semibold">Recommended Improvements</h2>
                 <p className="text-gray-600 text-sm mt-1">
                     Suggested actions to reduce manual effort and improve workflow efficiency
                 </p>
             </div>
 
-            <div className="space-y-3">
+            <div className="space-y-3 flex justify-center">
                 {recommendations.map((recommendation, index) => (
-                    <div key={index} className="border rounder-lg p-4">
+                    <div key={index} className="border rounder-lg p-4 w-1/2">
                         <div className="flex items-start gap-3">
                             <span>
                                 #{index + 1}
